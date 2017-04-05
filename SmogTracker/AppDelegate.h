ef96e9a7e092028e965c1f640adffaefcdab5913
admin <admin@MacBook-Air-admin.local>
2017-04-05 13:59:42 +0200

    Initial Commit

---
+++ SmogTracker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmogTracker
+//
+//  Created by admin on 05.04.2017.
+//  Copyright © 2017 pawel.ferens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmogTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SmogTracker
+//
+//  Created by admin on 05.04.2017.
+//  Copyright © 2017 pawel.ferens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
