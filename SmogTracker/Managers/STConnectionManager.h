11a06469a618a32b470dcfde338d6b88286d9c01
admin <admin@MacBook-Air-admin.local>
2017-04-05 14:27:59 +0200

    Initial commit

---
+++ SmogTracker/Managers/STConnectionManager.h
@@ -0,0 +1,13 @@
+//
+//  STConnectionManager.h
+//  SmogTracker
+//
+//  Created by admin on 05.04.2017.
+//  Copyright © 2017 pawel.ferens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STConnectionManager : NSObject
+
+@end
---
+++ SmogTracker/Managers/STConnectionManager.m
@@ -0,0 +1,13 @@
+//
+//  STConnectionManager.m
+//  SmogTracker
+//
+//  Created by admin on 05.04.2017.
+//  Copyright © 2017 pawel.ferens. All rights reserved.
+//
+
+#import "STConnectionManager.h"
+
+@implementation STConnectionManager
+
+@end
